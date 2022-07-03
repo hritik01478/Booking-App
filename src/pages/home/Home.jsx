1517ddaea09ecefc3559538587fa0c3b876c325b
hritik01478 <hritikkumar01478@gmail.com>
2022-07-03 15:32:11 +0530

    property list and featured properties made

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,8 @@
 import Featured from '../../components/featured/Featured';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
+import PropertyList from '../../components/propertyList/PropertyList';
 import './home.css';
 
 const Home = () => {
@@ -11,6 +13,9 @@ const Home = () => {
             <div className="homeContainer">
                 <Featured />
                 <h1 className="homeTitle">Browse by property type</h1>
+                <PropertyList />
+                <h1 className="homeTitle">Homes Guests love</h1>
+                <FeaturedProperties />
             </div>
         </div>
     )
